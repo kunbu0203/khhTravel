77e07240aa768357aa79dd3b759190d190f123e9
vivi.peng <vivi.peng@aplus-digital.com>
2021-03-10 14:21:27 +0800

    資料帶入 下拉篩選

--- codes/src/assets/js/main.js
+++ codes/src/assets/js/main.js
@@ -1,9 +1,136 @@
 var xhr = new XMLHttpRequest();
+var card = document.querySelector('#card'),
+    contentBox = document.querySelector('#cardBox');
 
 xhr.open('get', 'https://api.kcg.gov.tw/api/service/get/9c8e1450-e833-499c-8320-29b36b7ace5c', true)
 xhr.send(null)
 
 xhr.onload = function (){
     var obj = JSON.parse(xhr.responseText);
-    console.log(obj.contentType);
+    var data = obj.data.XML_Head.Infos.Info;
+    
+
+    for (var i = 0; i < data.length; i++) {
+        var el = data[i];
+        putContent(el);
+    }
+
+    contentBox.removeChild(card);
+
+
+    // 下拉篩選行政區 START
+    var searchBox = document.querySelector('#searchBox');
+
+    searchBox.addEventListener('change', function (e){
+        contentBox.innerHTML = '';
+        var val = this.value;
+
+        for (var i = 0; i < data.length; i++) {
+            var el = data[i];
+
+            if (val === el.Zipcode || val === ''){
+                putContent(el);
+            }
+        }
+    }, false);
+    // 下拉篩選行政區 END
+}
+
+function zipcode(code){
+    switch (code) {
+        case '800':
+            return '新興區';
+        case '801':
+            return '前金區';
+        case '802':
+            return '苓雅區';
+        case '803':
+            return '鹽埕區';
+        case '804':
+            return '鼓山區';
+        case '805':
+            return '旗津區';
+        case '806':
+            return '前鎮區';
+        case '807':
+            return '三民區';
+        case '811':
+            return '楠梓區';
+        case '812':
+            return '小港區';
+        case '813':
+            return '左營區';
+        case '814':
+            return '仁武區';
+        case '815':
+            return '大社區';
+        case '820':
+            return '岡山區';
+        case '821':
+            return '路竹區';
+        case '822':
+            return '阿蓮區';
+        case '823':
+            return '田寮區';
+        case '824':
+            return '燕巢區';
+        case '825':
+            return '橋頭區';
+        case '826':
+            return '梓官區';
+        case '827':
+            return '彌陀區';
+        case '828':
+            return '永安區';
+        case '829':
+            return '湖內區';
+        case '830':
+            return '鳳山區';
+        case '831':
+            return '大寮區';
+        case '832':
+            return '林園區';
+        case '833':
+            return '鳥松區';
+        case '840':
+            return '大樹區';
+        case '842':
+            return '旗山區';
+        case '843':
+            return '美濃區';
+        case '844':
+            return '六龜區';
+        case '845':
+            return '內門區';
+        case '846':
+            return '杉林區';
+        case '847':
+            return '甲仙區';
+        case '848':
+            return '桃源區';
+        case '849':
+            return '那瑪夏區';
+        case '851':
+            return '茂林區';
+        case '852':
+            return '茄萣區';
+        default:
+            break;
+    }
+}
+
+function putContent(el){
+    var cardClone = card.cloneNode(true);
+        
+    cardClone.querySelector('#cardImg').src = el.Picture1;
+    cardClone.querySelector('#cardName').textContent = el.Name;
+    cardClone.querySelector('#cardArea').textContent = zipcode(el.Zipcode);
+    cardClone.querySelector('#infoTime').textContent = el.Opentime;
+    cardClone.querySelector('#infoAddress').textContent = el.Add;
+    cardClone.querySelector('#infoPhone').textContent = el.Tel;
+    if (!el.Ticketinfo){
+        cardClone.querySelector('#infoTag').textContent = '免費參觀';
+    }
+
+    contentBox.appendChild(cardClone);
 }
